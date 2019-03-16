91c791e6fd89a89a769fb02bde6a65d9848cb02d
Nuno Gervasio <nunogee@gmail.com>
2019-03-16 13:26:18 +0100

    Updates

---
+++ precache-manifest.00629fd9f3499253f9d5fdf1ee897475.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "87320e11270c3d758e27",
+    "url": "/react-scoreboard/static/css/main.088b998e.chunk.css"
+  },
+  {
+    "revision": "87320e11270c3d758e27",
+    "url": "/react-scoreboard/static/js/main.875e8d97.chunk.js"
+  },
+  {
+    "revision": "da4941b842451d92ab2c",
+    "url": "/react-scoreboard/static/js/runtime~main.d9728706.js"
+  },
+  {
+    "revision": "85f54585fa6f49bcb418",
+    "url": "/react-scoreboard/static/js/2.148ed960.chunk.js"
+  },
+  {
+    "revision": "0db63bde6a8cca7d04ecbfa401245180",
+    "url": "/react-scoreboard/index.html"
+  }
+];
